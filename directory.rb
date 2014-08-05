5f5c9cc8aae3c97afe767f5d9e5b399d97d03a3f
James McNeil <james.r.mcneil@gmail.com>
2014-08-05 16:57:51 +0100

    Getting rid of some repetition

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-#first, we print the list of students
+#first, we create an array containing the students on our course.
 
  
 students = [
@@ -31,30 +31,9 @@ students = [
 puts "The students of my cohort at Makers Academy"
 puts "---------------"
 
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
-puts students[11]
-puts students[12]
-puts students[13]
-puts students[14]
-puts students[15]
-puts students[16]
-puts students[17]
-puts students[18]
-puts students[19]
-puts students[20]
-puts students[21]
-puts students[22]
-puts students[23]
-puts students[24]
+students.each do |student|
+	puts student
+end
 
 #finally, we print the total
 puts "Overall, we have #{students.length} great students"
