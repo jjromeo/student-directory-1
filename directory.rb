64bf8e077bad01414fbb98f51dc13e0825114ee7
James McNeil <james.r.mcneil@gmail.com>
2014-08-05 16:55:47 +0100

    Changing student list into an array

--- directory.rb
+++ directory.rb
@@ -1,30 +1,60 @@
-student_count = 24
 #first, we print the list of students
+
+ 
+students = [
+ "Javier Silverio",
+ "Elliot Lewis",
+ "Ben Tillett",
+ "Vincent Koch",
+ "Michelle Ballard",
+ "Nick Roberts",
+ "Tatiana Soukiassian",
+ "Mervé Silk",
+ "Albert Vallverdu",
+ "Lovis Schultze",
+"Henry Stanley",
+ "Spike Lindsey",
+ "Ruth Earle",
+ "Andy Gates",
+ "Faisal Aydarus",
+ "Ethel Ng",
+ "Kevin Daniells",
+ "Maya Driver",
+ "Leopold Kwok",
+ "James McNeil",
+ "Jerome Pratt",
+ "David Wickes",
+ "Chris Oatley",
+ "Marc Singh"
+]
+
 puts "The students of my cohort at Makers Academy"
 puts "---------------"
-puts "Javier Silverio"
-puts "Elliot Lewis"
-puts "Ben Tillett"
-puts "Vincent Koch"
-puts "Michelle Ballard"
-puts "Nick Roberts"
-puts "Tatiana Soukiassian"
-puts "Mervé Silk"
-puts "Albert Vallverdu"
-puts "Lovis Schultze"
-puts "Henry Stanley"
-puts "Spike Lindsey"
-puts "Ruth Earle"
-puts "Andy Gates"
-puts "Faisal Aydarus"
-puts "Ethel Ng"
-puts "Kevin Daniells"
-puts "Maya Driver"
-puts "Leopold Kwok"
-puts "James McNeil"
-puts "Jerome Pratt"
-puts "David Wickes"
-puts "Chris Oatley"
-puts "Marc Singh"
+
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+puts students[11]
+puts students[12]
+puts students[13]
+puts students[14]
+puts students[15]
+puts students[16]
+puts students[17]
+puts students[18]
+puts students[19]
+puts students[20]
+puts students[21]
+puts students[22]
+puts students[23]
+puts students[24]
+
 #finally, we print the total
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.length} great students"
